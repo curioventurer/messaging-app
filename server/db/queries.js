6172a2bc4e3debf8108f0ffae199e74f52225705
curioventurer <28566733+curioventurer@users.noreply.github.com>
2024-12-11 01:48:48 +0800

    feat: update group list when new messages arrive
    When new messages arrive:
    1. The last message displayed in groups item is updated.
    2. The groups list is sorted to show the updated
    group on top.
    
    It does not work however, if the client is the one
    sending the message. This is because the client only
    receives callback for sending the message, but not
    the emit event.
    This problem will be fixed in the next commit.

--- server/db/queries.js
+++ server/db/queries.js
@@ -51,23 +51,6 @@ async function getGroupsSummaryByUserId(userId) {
   }
   await Promise.all(promises);
 
-  groups.sort((a, b) => {
-    //Use group itself for sorting if lastMessage is null, otherwise use lastMessage.
-    const itemA = a.lastMessage ?? a;
-    const itemB = b.lastMessage ?? b;
-
-    /*Sort by new item first, using time followed by id.
-      If joined time is defined, item is a group and should use joined time,
-      else it is lastMessage and should use created time.
-    */
-    const timeA = new Date(itemA.joined ?? itemA.created).getTime();
-    const timeB = new Date(itemA.joined ?? itemB.created).getTime();
-    const timeDiff = timeB - timeA;
-
-    if (timeDiff !== 0) return timeDiff;
-    else return itemB.id - itemA.id;
-  });
-
   return groups;
 }
 
--- src/components/GroupList.jsx
+++ src/components/GroupList.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import GroupItem from "./GroupItem";
+import sortGroups from "../controllers/sortGroups.js";
 
 function GroupList() {
-  let [groups, setgroups] = useState([]);
+  let [groups, setGroups] = useState([]);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -11,7 +12,7 @@ function GroupList() {
 
     fetch(request)
       .then((res) => res.json())
-      .then((data) => setgroups(data))
+      .then((data) => setGroups(sortGroups(data)))
       .catch(() => {});
 
     return () => {
@@ -22,6 +23,32 @@ function GroupList() {
       );
     };
   }, []);
+
+  useEffect(() => {
+    window.socket.on("message", updateLastMsg);
+
+    return () => {
+      window.socket.off("message", updateLastMsg);
+    };
+  }, []);
+
+  function updateLastMsg(message) {
+    setGroups((prevGroups) => {
+      const index = prevGroups.findIndex(
+        (group) => group.id === message.group_id,
+      );
+      if (index === -1) return prevGroups;
+
+      const updatedGroup = { ...prevGroups[index], lastMessage: message };
+      const newGroups = sortGroups([
+        updatedGroup,
+        ...prevGroups.slice(0, index),
+        ...prevGroups.slice(index + 1),
+      ]);
+
+      return newGroups;
+    });
+  }
 
   return (
     <ul className="group-list left-screen">
